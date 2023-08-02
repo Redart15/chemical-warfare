bab5626682903ea16494f1a70d9e2536e12ce8ba
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-02 17:02:45 +0200

    changed what is effected by poison resistance and gasmask is now of type armor

--- data-updates/items/poison_res_armor.lua
+++ data-updates/items/poison_res_armor.lua
@@ -1,20 +1,13 @@
--- adds resistance to poison
-local list_armor = {"light-armor", "heavy-armor", "modular-armor", "power-armor", "power-armor-mk2"}
+-- adds poison resistance to armor using gasmasks
+local list_armor = {"power-armor", "power-armor-mk2"}
+local init_decrease = 4
+local init_resist = 20
 
 for index, value in ipairs(list_armor) do
-    local poison =  {type = "poison", percent = 10 * (index - 1), decrease = (index - 1) * index }
+    local poison =  {type = "poison", percent = init_resist * index, decrease = init_decrease}
     table.insert(data.raw["armor"][value]["resistances"],poison) 
+    table.insert(data.raw["recipe"][value]["ingredients"],{"gasmask",1})
 end
 
--- modifies recipe
-data.raw["recipe"]["light-armor"]["ingredients"] = {{"iron-plate",45}}
-data.raw["recipe"]["heavy-armor"]["ingredients"] = {{"copper-plate",120},{"steel-plate",50}}
-
-local elementsToSkip = 2
-for i = elementsToSkip + 1, #list_armor do
-    table.insert(data.raw["recipe"][list_armor[i]]["ingredients"],{"gasmask",1})
-end
-
--- modifies technology
 
 
--- data/items/gasmask.lua
+++ data/items/gasmask.lua
@@ -1,13 +1,21 @@
 local igasmask = {
-  type = "item",
+  type = "armor",
+  group = "combat",
+  subgroup = "armor",
+  order = "f",
   name = "gasmask",
+  stack_size = 1,
+  infinite = true,
+  resistances = {
+    {
+      type = "poison",
+      percent = 30,
+      decrease = 5,
+    },
+  },
   icon = data.raw["item"]["flying-robot-frame"].icon, -- need an actual icon for this
   icon_size = 64,
   icon_mipmaps = 4,
-  stack_size = 1,
-  group = "combat",
-  subgroup = "equipment",
-  order = "g[night-vision]",
 }
 
 local rgasmask = {
@@ -17,7 +25,7 @@ local rgasmask = {
   energy_required = 10,
   ingredients = {
     {"plastic-bar", 10},
-    {"advanced-circuit",5},
+    {"electronic-circuit",5},
     {"coal", 10},
   },
   result = "gasmask",
