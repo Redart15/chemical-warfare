7ad09e550af8f26045268d3cde86fd539743dc9f
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-03 22:17:18 +0200

    commented unsed requirements

--- data.lua
+++ data.lua
@@ -4,6 +4,7 @@ CAPSULE = ROOT .. "capsule."
 PROJECTILE = ROOT .. "projectile."
 RECIPE = ROOT .. "recipe."
 STREAM = ROOT .. "stream."
+STICKER = ROOT .. "sticker."
 TECHNOLOGY = ROOT .. "technology."
 
 --armor
@@ -14,13 +15,18 @@ require(CAPSULE .. "acid_capsule")
 
 --projectile
 require(PROJECTILE ..  "acid_cluster_projectile")
+--require(PROJECTILE .. "acid_fire_cluster_projectile")
 
 --recipe
 require(RECIPE .. "acid_capsule")
 require(RECIPE .. "gasmask")
 
+--sticker
+--require(STICKER .. "acid_fire_sticker")
+
 --stream
 require(STREAM .. "acid_stream")
+--require(STREAM ..  "acid_fire_stream")
 
 
 
