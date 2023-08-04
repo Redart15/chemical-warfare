eeda81c71447660f3950a0332a22601012f804bf
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-04 10:37:22 +0200

    no longer needed

--- prototypes/stream/acid_stream.lua
+++
@@ -1,6 +0,0 @@
-local acid_stream = table.deepcopy(data.raw.stream["acid-stream-spitter-big"])
-acid_stream.name = "acid-stream"
-acid_stream.initial_action[2].action_delivery.force = "all"
-acid_stream.initial_action[2].action_delivery.target_effects[2].amount = 20 -- does seem to do anything
-
-data:extend{acid_stream}
