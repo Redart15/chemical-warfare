510dfbbbc313c2b05aca42fc01f38ccaeab7c379
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-04 10:43:30 +0200

    deleted unused files

--- prototypes/unused/modified-fire-stream.lua
+++
@@ -1,4 +0,0 @@
-local modify_fire_stream = table.deepcopy(data.raw.stream["fire-stream"])
-local acid_stream = table.deepcopy(data.raw.stream["acid-stream-spitter-big"])
-
-modify_fire_stream.name = "test-stream"
--- prototypes/unused/ok.lua
+++
@@ -1,3 +0,0 @@
-local entity_name = data.raw.stream["acid-stream-spitter-big"].initial_action[1].action_delivery.target_effects[2].entity_name
-local splash = data.raw.entity[entity_name]
-print(splash)
