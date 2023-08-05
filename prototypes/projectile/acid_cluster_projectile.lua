c7d03a66f03e123e619074d6de04a75411bbcaf8
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-05 22:58:41 +0200

    simplyfied the code, and made sound loader

--- prototypes/projectile/acid_cluster_projectile.lua
+++ prototypes/projectile/acid_cluster_projectile.lua
@@ -1,67 +1,60 @@
-local capsule_explosion_sound = {
+local acp_capsule_explosion_sound = {
     {
         filename = "__base__/sound/fight/poison-capsule-explosion-1.ogg",
-        volume = 0.8
+        volume = 1.0
     },
     {
         filename = "__base__/sound/fight/poison-capsule-explosion-2.ogg",
-        volume = 0.8
+        volume = 1.0
     },
     {
         filename = "__base__/sound/fight/poison-capsule-explosion-3.ogg",
-        volume = 0.8
+        volume = 1.0
     },
     {
         filename = "__base__/sound/fight/poison-capsule-explosion-4.ogg",
-        volume = 0.8
+        volume = 1.0
     },
     {
         filename = "__base__/sound/fight/poison-capsule-explosion-5.ogg",
-        volume = 0.8
+        volume = 1.0
     }
 }
 
+local ac_target_sound = {
+    type = "play-sound",
+    sound = acp_capsule_explosion_sound
+}
 
-local acid_action = {
+local acp_acid_action = {
     type = "stream",
-    stream = "acid-stream-player",
+    stream = "acid-player-stream",
 }
 
 local acid_cluster_projectile = table.deepcopy(data.raw.projectile["slowdown-capsule"])
 acid_cluster_projectile.name = "acid-cluster-projectile"
 acid_cluster_projectile.action = {
+    -- {
+    --     type = "cluster",
+    --     distance = 2,
+    --     cluster_count = 5,
+    --     distance_deviation = 1,
+    --     action_delivery = acp_acid_action,
+    -- },
+    {
+        type = "area",
+        repeat_count = 8,
+        target_entities = false,
+        trigger_from_target = true,
+        radius = 3.5,
+        action_delivery = acp_acid_action,                      
+    },
     {
         type = "direct",
         action_delivery = {
             type = "instant",
-            target_effects = {
-                {
-                    type = "nested-result",
-                    action = 
-                    {
-                        {
-                            type = "cluster",
-                            distance = 2,
-                            cluster_count = 5,
-                            distance_deviation = 1,
-                            action_delivery = acid_action
-                        },
-                        {
-                            type = "area",
-                            repeat_count = 3,
-                            target_entities = false,
-                            trigger_from_target = true,
-                            radius = 4,
-                            action_delivery = acid_action,                      
-                        }
-                    }
-                },
-                {
-                    type = "play-sound",
-                    sound = capsule_explosion_sound
-                }
-            }
-        }
+            target_effects = ac_target_sound,
+        },
     }
 }
 
