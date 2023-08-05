b393b30ab9d5f930833e262687e11558f865bb30
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-05 23:00:33 +0200

    updated data to include all the new additions

--- data.lua
+++ data.lua
@@ -1,19 +1,27 @@
 ROOT = "prototypes."
 ARMOR = ROOT .. "armor."
+AMMO = ROOT .. "ammo."
 CAPSULE = ROOT .. "capsule."
+FIRE = ROOT .. "fire."
 PROJECTILE = ROOT .. "projectile."
 RECIPE = ROOT .. "recipe."
 STREAM = ROOT .. "stream."
 STICKER = ROOT .. "sticker."
 TECHNOLOGY = ROOT .. "technology."
-FIRE = ROOT .. "fire."
+
 
 -- armor
 require(ARMOR .. "gasmask")
 require(ARMOR .. "poison_res_armor")
 
+-- ammo
+require(AMMO .. "acidthrower-ammo")
+
 -- capsule
 require(CAPSULE .. "acid_capsule")
+
+-- fire
+require(FIRE .. "acid_splash_player_fire")
 
 --projectile
 require(PROJECTILE ..  "acid_cluster_projectile")
@@ -22,16 +30,15 @@ require(PROJECTILE ..  "acid_cluster_projectile")
 -- recipe
 require(RECIPE .. "acid_capsule")
 require(RECIPE .. "gasmask")
+require(RECIPE .. "acidthrower-ammo")
 
 -- sticker
--- require(STICKER .. "acid_fire_sticker")
+require(STICKER .. "acid_player_sticker")
 
 -- stream
-require(STREAM .. "acid_stream_player")
+require(STREAM .. "acidthrower_stream")
+require(STREAM .. "acid_capsule_stream")
 -- require(STREAM ..  "acid_fire_stream")
-
--- fire
-require(FIRE .. "acid_splash_fire_player")
 
 -- technology
 require(TECHNOLOGY .. "add_gasmask_2mil3")
