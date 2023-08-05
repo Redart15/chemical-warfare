231d3db39ff2a0ead88b9f5683de862f624cfffb
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-05 23:37:25 +0200

    reduced magazine size

--- prototypes/ammo/acidthrower-ammo.lua
+++ prototypes/ammo/acidthrower-ammo.lua
@@ -6,6 +6,7 @@ acidthrower_ammo.icons = {
         tint = {r=1,b=1,g=0,a=0.7},
     },
 }
+acidthrower_ammo.magazine_size = 20
 acidthrower_ammo.order = "d[flamethrower]"
 acidthrower_ammo.ammo_type[1].action.action_delivery.stream = "acidthrower-stream"
 
