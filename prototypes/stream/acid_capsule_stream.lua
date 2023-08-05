d191b73c88d26155d93f38c29299d4ce13d084e3
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-05 23:37:44 +0200

    made it so its does damage to all instead of enemy

--- prototypes/stream/acid_capsule_stream.lua
+++ prototypes/stream/acid_capsule_stream.lua
@@ -3,6 +3,7 @@ acid_capsule_stream.name = "acid-player-stream"
 
 
 acid_capsule_stream.initial_action[1].action_delivery.target_effects[2].entity_name = "acid-splash-player-fire"
+acid_capsule_stream.initial_action[2].force = "all"
 local asp_stream_effects = acid_capsule_stream.initial_action[2].action_delivery.target_effects
 asp_stream_effects[1].sticker = "acid-player-sticker"
 asp_stream_effects[2].damage = {amount = 90, type = "acid"}
