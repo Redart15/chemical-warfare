b9543e830ab959be0dc5c6a6c73c1e6cac6acdaa
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-07 00:12:05 +0200

    made vehical acidthwoer in tank use the same stream as the acidthrower

--- prototypes/ammo/acidthrower-ammo.lua
+++ prototypes/ammo/acidthrower-ammo.lua
@@ -9,5 +9,5 @@ acidthrower_ammo.icons = {
 acidthrower_ammo.magazine_size = 20
 acidthrower_ammo.order = "f[flamethrower]"
 acidthrower_ammo.ammo_type[1].action.action_delivery.stream = "acidthrower-stream"
-
+table.remove(acidthrower_ammo.ammo_type,2)
 data:extend({acidthrower_ammo})
