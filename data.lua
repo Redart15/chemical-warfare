e08782a85ffd35199d9d30f3655cd81db4ca6b11
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-10 14:04:53 +0200

    adjusted the data and language file

--- data.lua
+++ data.lua
@@ -7,30 +7,7 @@ ROOT = "prototypes."
 local gasmask = ROOT .. "gasmask-armor."
 require(gasmask .. "armor")
 require(gasmask .. "recipe")
-
-
-local base = ROOT .. "base-mod."
-require(base .. "poison-res-armor") -- adds poison resistance
-require(base .. "add-mil3-2power-armor") -- adds technology to power armour
-require(base .. "add-gasmask-2mil3")
-
--- acid-capsule
-local acid_capsule = ROOT .. "acid-capsule."
-require(acid_capsule .. "capsule")
-require(acid_capsule .. "projectile")
-require(acid_capsule .. "recipe")
-require(acid_capsule .. "stream")
-
--- added fire to used by player
-local fire = ROOT .. "acid-fire."
-require(fire .. "acid-splash-player-fire")
-
--- acidthrower-ammo
-local acidthrower = ROOT .. "acidthrower-ammo."
-require(acidthrower .. "ammo")
-require(acidthrower .. "recipe")
-require(acidthrower .. "sticker")
-require(acidthrower .. "stream")
+require(gasmask .. "technology")
 
 -- posion-rocket
 local poison_rocket = ROOT .. "poison-rocket."
@@ -38,10 +15,11 @@ require(poison_rocket .. "ammo")
 require(poison_rocket .. "projectile")
 require(poison_rocket .. "recipe")
 
+-- hazmat-armor
+local hazmat_armor = ROOT .. "hazmat-armor."
+require(hazmat_armor .. "armor")
+require(hazmat_armor .. "recipe")
 
-
-
-
-
-
-
+-- technologies
+local technologies = ROOT .. "technologies."
+require(technologies .. "chemical-warfare")
