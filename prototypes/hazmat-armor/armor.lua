b0601ce40049d2b61900cc49c637ee9b7394569a
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-10 14:01:46 +0200

    created hazmat suit

---
+++ prototypes/hazmat-armor/armor.lua
@@ -0,0 +1,63 @@
+-- -- adds poison resistance to armor using gasmasks
+-- local list_armor = { "power-armor", "power-armor-mk2" }
+-- local init_decrease = 4
+-- local init_resist = 30
+-- local ingredient = { "gasmask", 1 }
+
+-- for index, value in ipairs(list_armor) do
+--     local poison = {
+--         type = "poison",
+--         percent = init_resist * index,
+--         decrease = init_decrease * index,
+--     }
+--     table.insert(data.raw["armor"][value]["resistances"], poison)
+--     table.insert(data.raw["recipe"][value]["ingredients"], ingredient)
+-- end
+
+
+local hazmat_icon = table.deepcopy(data.raw.armor["power-armor-mk2"].icon)
+
+data:extend(
+    {
+
+        {
+            type = "armor",
+            group = "combat",
+            subgroup = "armor",
+            order = "faab",
+            name = "hazmat-suit-armor",
+            stack_size = 1,
+            infinite = true,
+            resistances = {
+                {
+                    type = "poison",
+                    percent = 60,
+                    decrease = 5,
+                },
+                {
+                    type = "acid",
+                    percent = 60,
+                    decrease = 5,
+                },
+                {
+                    type = "fire",
+                    percent = 60,
+                    decrease = 5,
+                },
+                {
+                    type = "electric",
+                    percent = 60,
+                    decrease = 5,
+                },
+            },
+            icons = {
+                {
+                    icon = hazmat_icon, -- need an actual icon for this
+                    tint = { r = 255 / 255, b = 0 / 255, g = 255 / 255, a = 0.6 },
+                },
+            },
+            icon_mipmaps = 4,
+            icon_size = 64,
+        },
+    }
+)
---
+++ prototypes/hazmat-armor/recipe.lua
@@ -0,0 +1,16 @@
+data:extend(
+    {
+        {
+            type = "recipe",
+            name = "hazmat-suit-armor",
+            enabled = false,
+            energy_required = 10,
+            ingredients = {
+                { "gasmask", 1 },
+                { "steel-plate", 30},
+                { "plastic-bar", 100}
+            },
+            result = "hazmat-suit-armor",
+        }
+    }
+)
