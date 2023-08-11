811b27ebc5696a81bf3d4e2dfcc6ce091fa7e066
Artem Betz <arbet100@uni-duesseldorf.de>
2023-08-11 15:28:09 +0200

    added localisation and adjusted data

--- data.lua
+++ data.lua
@@ -3,6 +3,9 @@ ROOT = "prototypes."
 
 -- sorting done by item to easily disable them.
 
+local bitter_breathing = ROOT .. "bitter-breathing."
+require(bitter_breathing .. "change-breathing")
+
 -- gasmask
 local gasmask = ROOT .. "gasmask-armor."
 require(gasmask .. "armor")
@@ -14,12 +17,10 @@ local poison_rocket = ROOT .. "poison-rocket."
 require(poison_rocket .. "ammo")
 require(poison_rocket .. "projectile")
 require(poison_rocket .. "recipe")
+require(poison_rocket .. "technology")
 
 -- hazmat-armor
 local hazmat_armor = ROOT .. "hazmat-armor."
 require(hazmat_armor .. "armor")
 require(hazmat_armor .. "recipe")
-
--- technologies
-local technologies = ROOT .. "technologies."
-require(technologies .. "chemical-warfare")
+require(hazmat_armor .. "technology")
